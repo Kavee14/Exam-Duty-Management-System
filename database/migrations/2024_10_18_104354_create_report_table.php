793139d93fe67a2e018798841c0d39cefc2a4886
pramodha99 <pramodhasithumini@gmail.com>
2024-10-23 12:30:01 +0530

    Update 2024_10_18_104354_create_report_table.php

--- database/migrations/2024_10_18_104354_create_report_table.php
+++ database/migrations/2024_10_18_104354_create_report_table.php
@@ -10,10 +10,12 @@ class CreateReportTable extends Migration
     {
         Schema::dropIfExists('report'); // Drops the table if it exists
         Schema::create('report', function (Blueprint $table) {
-            $table->increments('id'); // Example primary key
+            $table->increments('Report_ID'); // Example primary key
             $table->string('title'); // Example field
             $table->text('description'); // Example field
+            $table->integer('Duty_ID'); // Foreign Key column for Duty_ID
             // Add other fields as necessary
+            $table->index('Duty_ID', 'Lecturer_fk');
             $table->timestamps(); // Adds created_at and updated_at timestamps
         });
     }
