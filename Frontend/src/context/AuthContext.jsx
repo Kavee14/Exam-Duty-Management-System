27b88d268d79b1e8ca81e93f1914abd5673ba6ee
Kaveesha Dissanayake <dissanayake11435@usci.ruh.ac.lk>
2024-10-24 17:08:25 +0530

    Revert "LoginPage Frontend"

--- Frontend/src/context/AuthContext.jsx
+++
@@ -1,9 +0,0 @@
-import {createContext,useContext,useState} from 'react';
-import axios from '../api/axios';
-import {useNavigate} from "react-router-dom";
-
-const AuthContext = createContext({});
-
-export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState({null});
-}
